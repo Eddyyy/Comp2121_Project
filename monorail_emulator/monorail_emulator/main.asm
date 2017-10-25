0bc7f3f39e40b505cf58fbbb1b952638ed998cfb
Eddyyy <edward.thomson9@gmail.com>
2017-10-25 20:26:05 +1100

    re added lab4

--- monorail_emulator/monorail_emulator/main.asm
+++ monorail_emulator/monorail_emulator/main.asm
@@ -4,27 +4,19 @@
 ; Created: 20/10/2017 6:25:51 PM
 ; Author : Edward Thomsonn, Zhiwei Cao
 
-;IMPORTANT NOTICE: 
-;The labels on PORTL are reversed, i.e., PLi is actually PL7-i (i=0, 1, ¡­, 7).  
-
 ;Board settings: 
 ;Connect the four columns C0-C3 of the keypad to PL3-PL0 of PORTL and the four rows R0-R3 to PL7-PL4 of PORTL.
 ;For LCD data connect D0-D7 to PF0-PF7 respectively and for LCD commands connect BE,RW,E,RS to PA4-PA7 respectively
-    
-; For I/O registers located in extended I/O map, "IN", "OUT", "SBIS", "SBIC", 
-; "CBI", and "SBI" instructions must be replaced with instructions that allow access to 
-; extended I/O. Typically "LDS" and "STS" combined with "SBRS", "SBRC", "SBR", and "CBR".
 
 .include "m2560def.inc"
-.def row = r17
-.def col = r18
-.def mask = r19
-.def temp2 = r20
-.def temp = r21
-.def counter = r22
-.def buffer = r23
-.def flag = r9
-.def mode = r10
+.def temp =r22
+.def row =r17
+.def col =r18
+.def mask =r19
+.def temp2 =r20
+.def flag = r23
+.def counter = r24
+.def assci_zero =r21
 .equ PORTLDIR = 0xF0
 .equ INITCOLMASK = 0xEF
 .equ INITROWMASK = 0x01
@@ -41,16 +33,6 @@
 	rcall lcd_data
 	rcall lcd_wait
 .endmacro
-;A1 = A
-;A9 = I 
-;B1 = J
-;B9 = R
-;C1 = S
-;C8 = Z
-
-; mode = 0 Number Typing
-; mode = 1 Char Typing
-.dseg
 
 
 .cseg
@@ -73,9 +55,6 @@ out PORTF, temp
 out PORTA, temp
 
 clr counter
-clr flag
-clr mode
-clr buffer
 do_lcd_command 0b00111000 ; 2x5x7
 rcall sleep_5ms
 do_lcd_command 0b00111000 ; 2x5x7
@@ -88,139 +67,105 @@ do_lcd_command 0b00000110 ; increment, no display shift
 do_lcd_command 0b00001110 ; Cursor on, bar, no blink
 
 
-; keypad keeps scanning the keypad to find which key is pressed.
-keypad:
-	ldi mask, INITCOLMASK ; initial column mask
-	clr col ; initial column
+; main keeps scanning the keypad to find which key is pressed.
+main:
+ldi mask, INITCOLMASK ; initial column mask
+clr col ; initial column
 colloop:
-	STS PORTL, mask ; set column to mask value
-	; (sets column 0 off)
-
-	ldi temp, 0xFF ; implement a delay so the
-	; hardware can stabilize
+STS PORTL, mask ; set column to mask value
+; (sets column 0 off)
+ldi temp, 0xFF ; implement a delay so the
+; hardware can stabilize
 delay:
-	dec temp
-	brne delay
-
-	LDS temp, PINL ; read PORTL. Cannot use in 
-	andi temp, ROWMASK ; read only the row bits
-	cpi temp, 0xF ; check if any rows are grounded
-	breq nextcol ; if not go to the next column
-	ldi mask, INITROWMASK ; initialise row check
-	clr row ; initial row
-
-rowloop:
-	mov temp2, temp
-	and temp2, mask ; check masked bit
-	brne skipconv ; if the result is non-zero,
-	; we need to check again
-	rcall convert ; if bit is clear, convert the bitcode
-	jmp keypad ; and start again
+dec temp
+brne delay
+LDS temp, PINL ; read PORTL. Cannot use in 
+andi temp, ROWMASK ; read only the row bits
+cpi temp, 0xF ; check if any rows are grounded
+breq nextcol ; if not go to the next column
+ldi mask, INITROWMASK ; initialise row check
+clr row ; initial row
+rowloop:      
+mov temp2, temp
+and temp2, mask ; check masked bit
+brne skipconv ; if the result is non-zero,
+; we need to look again
+rcall convert ; if bit is clear, convert the bitcode
+jmp main ; and start again
 skipconv:
-	inc row ; else move to the next row
-	lsl mask ; shift the mask to the next bit
-	jmp rowloop          
+inc row ; else move to the next row
+lsl mask ; shift the mask to the next bit
+jmp rowloop          
 nextcol:     
-	cpi col, 3 ; check if we are on the last column
-	brne Continue ; if so, no buttons were pushed,
-	; so start again.
-	clr flag
-	jmp keypad
-
-
+cpi col, 3 ; check if we^Òre on the last column
+brne Continue ; if so, no buttons were pushed,
+; so start again.
+ldi flag,0
+rjmp main
 Continue:
-	sec ; else shift the column mask:
-	; We must set the carry bit
-	rol mask ; and then rotate left by a bit,
-	; shifting the carry into
-	; bit zero. We need this to make
-	; sure all the rows have
-	; pull-up resistors
-	inc col ; increment column value
-	jmp colloop ; and check the next column
-	; convert function converts the row and column given to a
-	; binary number and also outputs the value to PORTC.
-	; Inputs come from registers row and col and output is in
-	; temp.
+sec ; else shift the column mask:
+; We must set the carry bit
+rol mask ; and then rotate left by a bit,
+; shifting the carry into
+; bit zero. We need this to make
+; sure all the rows have
+; pull-up resistors
+inc col ; increment column value
+jmp colloop ; and check the next column
+; convert function converts the row and column given to a
+; binary number and also outputs the value to PORTC.
+; Inputs come from registers row and col and output is in
+; temp.
 convert:
-	cpi col, 3 ; if column is 3 we have a letter
-	breq letters
-	cpi row, 3 ; if row is 3 we have a symbol or 0
-	breq symbols
-;we need to change this to display letters when the numbers are pressed
-	mov temp, row ; otherwise we have a number (1-9)
-	lsl temp ; temp = row * 2
-	add temp, row ; temp = row * 3
-	add temp, col ; add the column address
-	; to get the offset from 1
-	inc temp ; add 1. Value of switch is
-	; row*3 + col + 1.
-	jmp branch
-
+cpi col, 3 ; if column is 3 we have a letter
+breq letters
+cpi row, 3 ; if row is 3 we have a symbol or 0
+breq symbols
+mov temp, row ; otherwise we have a number (1-9)
+lsl temp ; temp = row * 2
+add temp, row ; temp = row * 3
+add temp, col ; add the column address
+; to get the offset from 1
+inc temp ; add 1. Value of switch is
+; row*3 + col + 1.
+jmp number_convert
 letters:
-	ldi temp, 0b01000001
-	add temp, row ; increment from 0xA by the row value
-	jmp branch
+ldi temp, 0b01000001
+add temp, row ; increment from 0xA by the row value
+jmp convert_end
 symbols:
-	cpi col, 0 ; check if we have a star
-	breq star
-	cpi col, 1 ; or if we have zero
-	breq zero
-	ldi temp, 0b00100011 ; we'll output 0xF for hash
-	jmp branch
+cpi col, 0 ; check if we have a star
+breq star
+cpi col, 1 ; or if we have zero
+breq zero
+ldi temp, 0b00100011 ; we'll output 0xF for hash
+jmp convert_end
 star:
-	ldi temp, 0b00101010 ; we'll output 0xE for star
-	jmp branch
+ldi temp, 0b00101010 ; we'll output 0xE for star
+jmp convert_end
 zero:
-	clr temp ; set to zero
-	jmp branch
+clr temp ; set to zero
+jmp number_convert
 
 
-branch:
-	sbrs flag, 0		;cpi flag, 1
-	rjmp convert_ret
-	sbrs mode, 0	;cpi mode, 1
-	rjmp character_mode
-	rjmp number_mode
-
-character_mode:
-	cpi buffer, 0
-	cpi temp, 0b01000100
-	breq end_input
-	breq compare_A
-	cpi temp, 10
-	brlo load_values
-	rjmp compare_A
-load_values:
-	add temp, buffer
-	clr buffer
-	inc counter
-	do_lcd_data temp
-	rjmp convert_end
-compare_A:
-	cpi temp, 0b01000001
-	brne compare_B
-	ldi buffer,0b01000000; A-1
-	rjmp convert_end
-compare_B:
-	cpi temp, 0b01000010
-	brne compare_C
-	ldi buffer,0b01001001; J-1
-	rjmp convert_end
-compare_C:
-	cpi temp, 0b01000011
-	brne convert_end
-	ldi buffer,0b01010010; S-1
-	rjmp convert_end
-number_mode:
+number_convert:
+ldi assci_zero, 0x30
+add temp, assci_zero
 
 convert_end:
-	inc flag
-;	inc counter
-;	do_lcd_data temp
-
-end_input:
-
+cpi counter,17 ; 16 is maximum number of LCD
+brlo not_clean_line
+ldi counter, 1
+do_lcd_command 0b00000001 ; clear display
+do_lcd_command 0b00001110 ; Cursor on, bar, no blink
+do_lcd_data temp
+rjmp convert_ret
+not_clean_line:
+cpi flag, 1
+breq convert_ret
+ldi flag, 1
+inc counter
+do_lcd_data temp
 convert_ret:
 ret ; return to caller
 
@@ -317,7 +262,7 @@ sleep_5ms:
 	rcall sleep_1ms
 	rcall sleep_1ms
 	ret
-	
+
 ; Generated by delay loop calculator
 ; at http://www.bretmulvey.com/avrdelay.html
 ;
@@ -344,13 +289,14 @@ L1: dec  r20
 	pop r23
 	ret
 
+
 get_str: ;(mode=r17, &result=X)
 	;while(c<10 or temp != D
 	rcall keypad
 	;do logic on temp takes into account mode
 	display temp ;if temp != A,B,C,D
 	;if temp == D && mode == 1 --> store ';'
-
+	ret
 
 display_message: ;(&message)
 
@@ -362,5 +308,3 @@ main:
 	clr mode
 	rcall get_str ;return result
 	store_result;(&result, &config_array)
-
-	;for(i=0;i<result;i++)
